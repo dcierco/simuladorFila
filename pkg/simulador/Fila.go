30712035edd205cd0aeaa6c31053a760600d3d2e
Daniel Cierco <daniel_cierco@live.com>
2022-04-16 16:13:40 -0300

    criando estrutura básica de simulador e fila

--- pkg/simulador/Fila.go
+++ pkg/simulador/Fila.go
@@ -4,5 +4,40 @@ type Fila struct {
 	tempoChegada,
 	tempoAtendimento,
 	numeroServidores,
-	capacidade int
+	capacidade,
+	qtdeNaFila int
+	fila []float32
+}
+
+func NewFila(tempoChegada, tempoAtendimento, numeroServidores, capacidade int) *Fila {
+	p := new(Fila)
+	p.tempoChegada = tempoChegada
+	p.tempoAtendimento = tempoAtendimento
+	p.numeroServidores = numeroServidores
+	p.capacidade = capacidade
+	p.fila = make([]float32, 0, capacidade)
+	p.qtdeNaFila = 0
+	return p
+}
+
+func (fila Fila) estaVazia() bool {
+	if fila.qtdeNaFila == 0 {
+		return true
+	}
+	return false
+}
+
+func (fila Fila) estaCheia() bool {
+	if fila.qtdeNaFila == fila.capacidade {
+		return true
+	}
+	return false
+}
+
+func (receiver Fila) adicionaFila() {
+
+}
+
+func (receiver Fila) removeFila() {
+
 }
--- pkg/simulador/simulador.go
+++ pkg/simulador/simulador.go
@@ -1,1 +1,36 @@
 package simulador
+
+type simulador struct {
+	tempoAtual float64
+}
+
+func chegada(fila Fila) {
+	contabilizaTempo()
+	if fila.estaVazia() {
+		fila.adicionaFila()
+		if fila.qtdeNaFila <= fila.numeroServidores {
+			agendaSaida()
+		}
+	} else {
+		//perda++
+	}
+	agendaChegada()
+}
+
+func saida(fila Fila) {
+	contabilizaTempo()
+	fila.removeFila()
+
+}
+
+func contabilizaTempo() {
+	panic("unimplemented")
+}
+
+func agendaChegada() {
+	panic("unimplemented")
+}
+
+func agendaSaida() {
+	panic("unimplemented")
+}
